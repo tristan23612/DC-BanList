ef7a01ec83306a9c3704e213d8745c5e9a47285f
az5078 <tristan23612@gmail.com>
2025-08-21 03:07:26 +0900

    fix: typo

--- DC_BanList.js
+++ DC_BanList.js
@@ -3,7 +3,7 @@
 // @name:ko          디시인사이드 차단 내역 관리
 // @namespace        https://github.com/tristan23612/DC-BanList
 // @author           망고스틴
-// @version          1.0.7-dev
+// @version          1.0.7-dev.1
 // @description      디시인사이드 차단 내역 관리
 // @description:ko   디시인사이드 차단 내역 관리
 // @match            https://gall.dcinside.com/*/board/lists*
