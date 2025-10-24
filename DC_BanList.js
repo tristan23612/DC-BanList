6d59e4a328918bbe1a44125ee6d16b385bb9ff66
tristan23612 <tristan23612@gmail.com>
2025-10-25 02:04:05 +0900

    fix: 시트 Id 전달 수정

--- DC_BanList.js
+++ DC_BanList.js
@@ -3,7 +3,7 @@
 // @name:ko          디시인사이드 차단 내역 관리
 // @namespace        https://github.com/tristan23612/DC-BanList
 // @author           망고스틴
-// @version          1.2.3-release
+// @version          1.2.4-release
 // @description      디시인사이드 차단 내역 관리
 // @description:ko   디시인사이드 차단 내역 관리
 // @match            https://gall.dcinside.com/*/board/lists*
@@ -353,6 +353,7 @@ class ModalManager {
             else if (currentStep === 'UploadComplete') {
                 contentDiv.innerHTML = this.#uiManager.renderBanExportModalContent({
                     currentStep: currentStep,
+                    sheetId: storedSheetId,
                     resultMessage
                 });
                 footer.style.display = 'none';
